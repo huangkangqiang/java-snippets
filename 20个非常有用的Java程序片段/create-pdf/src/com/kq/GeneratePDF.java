48e13449213880c414595ef3b796cd9d2c7beed1
huangkangqiang <243286916@qq.com>
2017-07-31 23:51:35 +0800

    使用iText JAR生成PDF

--- 20个非常有用的Java程序片段/create-pdf/src/com/kq/GeneratePDF.java
+++ 20个非常有用的Java程序片段/create-pdf/src/com/kq/GeneratePDF.java
@@ -3,17 +3,30 @@ package com.kq;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.Date;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
 public class GeneratePDF {
 
 	public static void main(String[] args) {
-		
+
 		try {
-			OutputStream out=new FileOutputStream(new File("src/com/kq/test.pdf"));
-			
+			OutputStream out = new FileOutputStream(new File("src/com/kq/test.pdf"));
+
+			Document document = new Document();
+			PdfWriter.getInstance(document, out);
+			document.open();
+			document.add(new Paragraph("Hello World!"));
+			document.add(new Paragraph(new Date().toString()));
+
+			document.close();
+			out.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
